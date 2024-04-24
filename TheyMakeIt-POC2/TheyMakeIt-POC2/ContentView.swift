0d00257d8455ebaae4bd8f81c8b4d75fc5694801
Amandine Cousin <cousin.amandine.pro@gmail.com>
2024-04-24 15:58:23 +0200

    [POC2][P2C1] Corrigé de l'exercice avec les composants, les stacks et les marges

--- TheyMakeIt-POC2/TheyMakeIt-POC2/ContentView.swift
+++ TheyMakeIt-POC2/TheyMakeIt-POC2/ContentView.swift
@@ -12,10 +12,47 @@ struct ContentView: View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+                .foregroundStyle(.orange)
+            
+            Text("Hedy Lamarr")
+                .padding()
+            
+            HStack {
+                VStack {
+                    Image(systemName: "calendar")
+                        .padding(.bottom, 4)
+                    Text("1914")
+                    Text("-")
+                    Text("2020")
+                }
+                .padding()
+                
+                VStack {
+                    Image(systemName: "bookmark")
+                        .padding(.bottom, 4)
+                    Text("Actrice")
+                    Text("Inventrice")
+                }
+                .padding()
+                
+                VStack {
+                    Image(systemName: "mappin.and.ellipse")
+                       .padding(.bottom, 4)
+                    Text("Autrichienne")
+                    Text("Américaine")
+                }
+                .padding()
+            }
+
+            VStack {
+                Text("Télécommunications")
+                Text("Ingénierie aéronautique")
+            }
+            .padding()
+            
+            Text("Hedy Lamarr était une actrice autrichienne qui a également été une inventrice prolifique. Elle est surtout connue pour son invention avec George Antheil, du système de saut de fréquence, une technologie fondamentale pour les communications sans fil modernes.")
+                .padding()
         }
-        .padding()
     }
 }
 
