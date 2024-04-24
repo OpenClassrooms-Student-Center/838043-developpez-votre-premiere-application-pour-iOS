64e89af720583e2a8be5e3ba5f50c7c015b74489
Amandine Cousin <cousin.amandine.pro@gmail.com>
2024-04-24 15:42:52 +0200

    [POC1][P2C1] Ajout des VStacks

--- TheyMakeIt-POC1/TheyMakeIt-POC1/ContentView.swift
+++ TheyMakeIt-POC1/TheyMakeIt-POC1/ContentView.swift
@@ -13,12 +13,22 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.orange)
-            Text("Hedy Lamarr")
-            Text("1914 - 2020")
-            Text("Actrice Inventrice")
-            Text("Autrichienne Américaine")
-            Text("Télécommunications")
-            Text("Ingénierie aéronautique")
+            
+            VStack {
+                Text("Hedy Lamarr")
+                Text("1914 - 2020")
+            }
+            
+            VStack {
+                Text("Actrice Inventrice")
+                Text("Autrichienne Américaine")
+            }
+            
+            VStack {
+                Text("Télécommunications")
+                Text("Ingéniérie aéronautique")
+            }
+            
             Text("Hedy Lamarr était une actrice autrichienne qui a également été une inventrice prolifique. Elle est surtout connue pour son invention avec George Antheil, du système de saut de fréquence, une technologie fondamentale pour les communications sans fil modernes.")
         }
     }
