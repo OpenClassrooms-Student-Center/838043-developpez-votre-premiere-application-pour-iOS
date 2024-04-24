104ebe511702f781909f44f71558beefac50ecad
Amandine Cousin <cousin.amandine.pro@gmail.com>
2024-04-24 15:30:34 +0200

    [POC1] Initialisation du projet du cours TheyMakeIt-POC1 en Swift et SwiftUI

---
+++ TheyMakeIt-POC1/TheyMakeIt-POC1/TheyMakeIt_POC1App.swift
@@ -0,0 +1,17 @@
+//
+//  TheyMakeIt_POC1App.swift
+//  TheyMakeIt-POC1
+//
+//  Created by Amandine Cousin on 23/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct TheyMakeIt_POC1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
