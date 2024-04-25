d8c21b440af7de533eceaa30c601f115bf280662
Amandine Cousin <cousin.amandine.pro@gmail.com>
2024-04-25 10:57:33 +0200

    [POC1][P2C2] Customisation des textes avec round rectangle et clip shape + utilisation de geometry reader pour la taille de l'image.

--- TheyMakeIt-POC1/TheyMakeIt-POC1/ContentView.swift
+++ TheyMakeIt-POC1/TheyMakeIt-POC1/ContentView.swift
@@ -9,58 +9,66 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image("Hedy")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 400)
-            
+        GeometryReader { geometry in
             VStack {
-                Text("Hedy Lamarr")
-                    .font(.title)
-                    .fontWeight(.bold)
-                HStack {
-                    Text("1914")
-                    Text("-")
-                    Text("2020")
-                }
-                .font(.headline)
-                .foregroundStyle(.darkGrey)
-            }
-            .padding()
-            
-            VStack {
-                HStack {
-                    Text("Actrice")
-                    Text("Inventrice")
-                }
-                .font(.title3)
+                Image("Hedy")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: geometry.size.width)
                 
-                HStack {
-                    Text("Autrichienne")
-                    Text("Américaine")
+                VStack {
+                    Text("Hedy Lamarr")
+                        .font(.title)
+                        .bold()
+                    HStack {
+                        Text("1914")
+                        Text("-")
+                        Text("2020")
+                    }
+                    .font(.headline)
+                    .foregroundStyle(.darkGrey)
                 }
-                .foregroundStyle(.darkGrey)
-            }
-            
-            VStack {
-                Text("Télécommunications")
-                    .fontWeight(.bold)
-                    .background(.purple)
-                    .foregroundStyle(.white)
-                Text("Ingénierie aéronautique")
-                    .fontWeight(.bold)
-                    .background(.purple)
-                    .foregroundStyle(.white)
-            }
-            .padding()
-            
-            Text("Hedy Lamarr était une actrice autrichienne qui a également été une inventrice prolifique. Elle est surtout connue pour son invention avec George Antheil, du système de saut de fréquence, une technologie fondamentale pour les communications sans fil modernes.")
-                .font(.callout)
                 .padding()
+                .background(RoundedRectangle(cornerRadius: 10)
+                    .stroke(.purple, lineWidth: 2))
+                
+                VStack {
+                    HStack {
+                        Text("Actrice")
+                        Text("Inventrice")
+                    }
+                    .font(.title3)
+                    
+                    HStack {
+                        Text("Autrichienne")
+                        Text("Américaine")
+                    }
+                    .foregroundStyle(.darkGrey)
+                }
+                
+                VStack {
+                    Text("Télécommunications")
+                        .bold()
+                        .padding(8)
+                        .background(RoundedRectangle(cornerRadius: 10).fill(.purple))
+                        .foregroundStyle(.white)
+                    Text("Ingénierie aéronautique")
+                        .bold()
+                        .padding(8)
+                        .background(.purple)
+                        .foregroundStyle(.white)
+                        .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
+                }
+                .padding()
+                
+                Text("Hedy Lamarr était une actrice autrichienne qui a également été une inventrice prolifique. Elle est surtout connue pour son invention avec George Antheil, du système de saut de fréquence, une technologie fondamentale pour les communications sans fil modernes.")
+                    .font(.callout)
+                    .padding()
+            }
         }
     }
 }
+
 
 #Preview {
     ContentView()
