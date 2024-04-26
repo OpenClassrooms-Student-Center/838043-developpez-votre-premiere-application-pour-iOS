04d8a1881b68f114c9468064fbde6ab9c19eeeda
Amandine Cousin <cousin.amandine.pro@gmail.com>
2024-04-26 16:12:55 +0200

    [POC2][P2C4] Corrigé de l'exercice final : Ajout du tableau de la liste des femmes avec l'affichage de la liste et la navigation entre les 2 écrans

--- TheyMakeIt-POC2/TheyMakeIt-POC2/ContentView.swift
+++ TheyMakeIt-POC2/TheyMakeIt-POC2/ContentView.swift
@@ -8,15 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    let femme: Femme
+    
     var body: some View {
         GeometryReader { geometry in
             VStack {
-                Image("Hedy")
+                Image(femme.nomImage)
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                     .frame(width: geometry.size.width)
                 
-                Text("Hedy Lamarr")
+                Text(femme.nom)
                     .font(.system(size: 38))
                     .fontWeight(.heavy)
                     .foregroundStyle(.purple)
@@ -25,11 +27,11 @@ struct ContentView: View {
                     VStack {
                         Image(systemName: "calendar")
                             .padding(.bottom, 4)
-                        Text("1914")
+                        Text(femme.anneeNaissance)
                             .font(.callout)
                         Text("-")
                             .font(.callout)
-                        Text("2020")
+                        Text(femme.anneeDeces)
                             .font(.callout)
                     }
                     .frame(width: geometry.size.width/4, height: 100)
@@ -40,10 +42,10 @@ struct ContentView: View {
                     VStack {
                         Image(systemName: "bookmark")
                             .padding(.bottom, 4)
-                        Text("Actrice")
-                            .font(.callout)
-                        Text("Inventrice")
-                            .font(.callout)
+                        ForEach(femme.metiers, id: \.self) { metier in
+                            Text(metier)
+                                .font(.callout)
+                        }
                     }
                     .frame(width: geometry.size.width/3, height: 100)
                     .padding(4)
@@ -53,10 +55,10 @@ struct ContentView: View {
                     VStack {
                         Image(systemName: "mappin.and.ellipse")
                             .padding(.bottom, 4)
-                        Text("Autrichienne")
-                            .font(.callout)
-                        Text("Américaine")
-                            .font(.callout)
+                        ForEach(femme.nationalites, id: \.self) { nationalite in
+                            Text(nationalite)
+                                .font(.callout)
+                        }
                     }
                     .frame(width: geometry.size.width/4, height: 100)
                     .padding(4)
@@ -65,21 +67,17 @@ struct ContentView: View {
                 }
                 
                 VStack {
-                    Text("Télécommunications")
-                        .fontWeight(.bold)
-                        .padding(8)
-                        .background(RoundedRectangle(cornerRadius: 10).fill(.purple))
-                        .foregroundStyle(.white)
-                    Text("Ingénierie aéronautique")
-                        .fontWeight(.bold)
-                        .padding(8)
-                        .background(.purple)
-                        .foregroundStyle(.white)
-                        .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
+                    ForEach(femme.reussites, id: \.self) { reussite in
+                        Text(reussite)
+                            .fontWeight(.bold)
+                            .padding(8)
+                            .background(RoundedRectangle(cornerRadius: 10).fill(.purple))
+                            .foregroundStyle(.white)
+                    }
                 }
                 .padding()
                 
-                Text("Hedy Lamarr était une actrice autrichienne qui a également été une inventrice prolifique. Elle est surtout connue pour son invention avec George Antheil, du système de saut de fréquence, une technologie fondamentale pour les communications sans fil modernes.")
+                Text(femme.description)
                     .font(.callout)
                     .padding()
             }
@@ -88,5 +86,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    ContentView(femme: Femme(nom: "Ada Lovelace", metiers: ["Développeuse", "Poétesse"], nomImage: "Ada", anneeNaissance: "1815", anneeDeces: "1852", nationalites: ["Britanique"], reussites: ["Première développeuse", "Calculs numériques"], description: "Elle est principalement connue pour avoir réalisé le premier véritable programme informatique, lors de son travail sur un ancêtre de l'ordinateur : la machine analytique de Charles Babbage. Sa vision avant-gardiste de l'utilisation des ordinateurs et des algorithmes a ouvert la voie à l'ère de l'informatique moderne, faisant d'elle une figure emblématique de la technologie."))
 }
--- TheyMakeIt-POC2/TheyMakeIt-POC2/TheyMakeIt_POC2App.swift
+++ TheyMakeIt-POC2/TheyMakeIt-POC2/TheyMakeIt_POC2App.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct TheyMakeIt_POC2App: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ListingView()
         }
     }
 }
