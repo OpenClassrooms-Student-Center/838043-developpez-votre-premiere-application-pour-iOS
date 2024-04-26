8e435175af4d705da87866065d7f0b9d4ae12afa
Amandine Cousin <cousin.amandine.pro@gmail.com>
2024-04-26 15:59:15 +0200

    [POC2][P2C4] Création du fichier Model pour la structure Femme et du fichier ListingView pour la vue de la liste

---
+++ TheyMakeIt-POC2/TheyMakeIt-POC2/ListingView.swift
@@ -0,0 +1,18 @@
+//
+//  ListingView.swift
+//  TheyMakeIt-POC2
+//
+//  Created by Amandine Cousin on 26/04/2024.
+//
+
+import SwiftUI
+
+struct ListingView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    ListingView()
+}
